2d1c0ba8e8874c31a0710fdbe6cbed2c37b651ad
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-16 09:22:24 +0100

    access data from MySQL using mysql2 package and nodejs

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 const { faker } = require("@faker-js/faker");
+
+const mysql = require("mysql2");
+
+// create connection to database
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "insta_app",
+  password: "mysql@123",
+});
+
+// query
+let q = `SELECT * FROM user`;
+
+try {
+  connection.query(q, (err, result) => {
+    if (err) throw err;
+    console.log("result: ", result);
+  });
+} catch (err) {
+  console.log(err);
+}
+
+// close connection
+connection.end();
+
 const randomdata = () => {
   return [
     faker.string.uuid(),
@@ -9,4 +35,4 @@ const randomdata = () => {
   ];
 };
 
-console.log(randomdata());
+// console.log(randomdata());
