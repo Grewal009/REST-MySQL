5b3bdd5f47390cabe3476bf3c7d10d3b21662857
Aneel Grewal <aneel.grewal@hotmail.com>
2023-11-16 13:22:24 +0100

    implement PATCH /user/:id route

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express();
 let port = "8080";
 
 const path = require("path");
+const methodOverride = require("method-override");
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
+app.use(methodOverride("_method"));
+
+app.use(express.urlencoded({ extended: true }));
 
 const mysql = require("mysql2");
 
@@ -41,6 +45,52 @@ app.get("/user", (req, res) => {
       if (err) throw err;
       //res.send(result);
       res.render("showusers.ejs", { result });
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("error in DB");
+  }
+});
+
+app.get("/user/:id/edit", (req, res) => {
+  let { id } = req.params;
+  console.log("id: ", id);
+  let q = `SELECT * FROM user WHERE id = "${id}" `;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      let user = result[0];
+      res.render("edit.ejs", { user });
+    });
+  } catch (err) {
+    console.log(err);
+    res.send("error in DB");
+  }
+});
+
+app.patch("/user/:id/", (req, res) => {
+  let { id } = req.params;
+  let { username, password } = req.body;
+  console.log("new data submitted------>", username + " " + password);
+
+  let q = `SELECT * FROM user WHERE id = "${id}" `;
+  try {
+    connection.query(q, (err, result) => {
+      if (err) throw err;
+      console.log(result);
+      let user = result[0];
+      console.log("data from database------>>>", user.username, user.password);
+      if (user.password == password) {
+        let q2 = `UPDATE user SET username = "${username}" WHERE id="${id}"`;
+        connection.query(q2, (err, result) => {
+          if (err) throw err;
+          console.log("result ------>", result);
+          res.redirect("/user");
+        });
+      } else {
+        res.send("wrong password");
+      }
     });
   } catch (err) {
     console.log(err);
